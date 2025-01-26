ec9c864b03ecc9bbdae1307cb645bc3f1c9af1b7
MD.Tanvir Rahman <tanviredu2018@gmail.com>
2025-01-26 10:46:17 +0000

    added

--- DataLake/DATALAKE_UNSTRUCTURAL/main.py
+++ DataLake/DATALAKE_UNSTRUCTURAL/main.py
@@ -22,4 +22,43 @@ def read_data(file_path):
         raise ValueError("[-] Unknown Format. exiting")
 
 def perform_data_operation(data,layer):
-    pass
+    print("Performing data operations for {} Layer".format(layer))
+    if layer   == "refined":
+        data['Processed'] = True
+    elif layer == "curated":
+        if 'FirstName' in data.columns:
+            ## upper case conversion
+            ## consider this is a preprocessing
+            data['FirstName'] = data['FirstName'].apply(lambda x:x.upper())
+    return data
+
+
+def MainEx():
+    project_path     = os.path.dirname(os.path.abspath(__file__))
+    data_path        = os.path.join(project_path,"DATA")
+    lake_path        = os.path.join(data_path,"lake")
+    raw_path         = os.path.join(data_path,"raw")
+    refined_path     = os.path.join(data_path,"refined")
+    curated_path     = os.path.join(data_path,"curated")
+    #print(raw_path)
+    #print(refined_path)
+    #print(curated_path)
+
+    ## create directory
+    for layer_path in [raw_path,refined_path,curated_path]:
+        os.makedirs(layer_path,exist_ok=True)
+
+    
+    ## first part of the preprocessing we are moving data from lake to raw folder
+    ingest_data(lake_path,raw_path)
+
+    for item in os.listdir(raw_path):
+        file_path = os.path.join(raw_path,item)
+        data = read_data(file_path)
+        refined_data = perform_data_operation(data,"refined")
+        refined_out_path = os.path.join(refined_path,item)
+        print(refined_data)
+
+
+
+MainEx()
