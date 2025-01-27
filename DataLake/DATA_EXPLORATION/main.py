b0ec3dff879020d3015b5785b2cd52cf3a48335d
MD.Tanvir Rahman <tanviredu2018@gmail.com>
2025-01-27 08:16:52 +0000

    added

--- DataLake/DATA_EXPLORATION/main.py
+++ DataLake/DATA_EXPLORATION/main.py
@@ -1,5 +1,7 @@
 import pandas as pd
-from statistics import mean 
+from statistics import mean
+import seaborn as sns
+import matplotlib.pyplot as plt
 ## assignment1
 df = pd.read_csv("./LAKE/data.csv")
 print(df.head(5))
@@ -39,4 +41,23 @@ df2 = {
 print(df2)
 
 def age_group(age):
-    if age>=18 and 
+    if age>=18 and age <=25:
+        return "18-25"
+    elif age>=26 and age <=35:
+        return "26-35"
+    elif age>=36 and age <=45:
+        return "36-45"
+    else:
+        return "Unknown"
+
+df["Category"] = df['Age'].apply(age_group)
+
+print(df)
+
+sns.countplot(x='Category', hue='Gender', data=df, palette='coolwarm')
+plt.title("Age Groups by Gender")
+plt.xlabel("Age Group")
+plt.ylabel("Count")
+plt.legend(title="Gender")
+plt.show()
+plt.savefig("age_groups_by_gender.png", dpi=300, bbox_inches='tight') 
