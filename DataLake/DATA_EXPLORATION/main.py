3717ccdc3c5a9a0cd35f1a2acf0a5a7317382994
MD.Tanvir Rahman <tanviredu2018@gmail.com>
2025-01-27 06:57:13 +0000

    added

---
+++ DataLake/DATA_EXPLORATION/main.py
@@ -0,0 +1,42 @@
+import pandas as pd
+from statistics import mean 
+## assignment1
+df = pd.read_csv("./LAKE/data.csv")
+print(df.head(5))
+
+
+
+
+## assignment2.1
+female_customer = df[df['Gender']=="Female"]
+Male_customer = df[df['Gender']=="Male"]
+
+print("Average Age of Female Customer: {}".format(mean(female_customer['Age'])))
+print("Average Age of Male Customer: {}".format(mean(Male_customer['Age'])))
+
+rows = len(df.index)
+print("Rows of the dataframe is : {}".format(rows))
+columns = len(df.columns)
+print("Columns of the dataframe is : {}".format(columns))
+
+
+
+## assignmnt 3
+## total purchage amount
+
+TPA = sum(df['PurchaseAmount'])
+print("Total Purchage amount is : {}".format(TPA))
+
+
+product_sales = df.groupby(['ProductID'])['PurchaseAmount'].sum()
+print(product_sales)
+most_pur_product = product_sales.idxmax()
+most_pur_product_sales = product_sales.max()
+df2 = {
+    "product":float(most_pur_product),
+    "sales": float(most_pur_product_sales)
+}
+print(df2)
+
+def age_group(age):
+    if age>=18 and 
